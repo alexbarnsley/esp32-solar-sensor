348d4cef698ed5caba084ab0f76c97dcacd3efb4
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-10-18 15:29:54 +0100

    chore: scrub

--- boot.py
+++ boot.py
@@ -1,8 +1,4 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
 
 import utime
 
@@ -11,4 +7,3 @@ print('Starting...')
 utime.sleep(30)
 
 import base
-#import test_wifi
